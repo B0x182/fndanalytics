fb6e03979f3c96792bdc99f684ab4fa2ccf17329
B0x182 <97475421+B0x182@users.noreply.github.com>
2022-01-15 14:47:22 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,30 @@
 window.onload = function (){
 
+ var chart0 = new Chart(
+    document.getElementById('marketVolumeChart'),
+    getStackedBarChartConfig()
+  );
+
  var chart1 = new Chart(
-    document.getElementById('finalizedMarketAuctionsChart'),
+    document.getElementById('marketStatsChart'),
     getBarChartConfig()
   );
 
-initFNDanalytics().then((result) => {
+initFNDanalytics().then((r0) => {
+ 
+	getMarketVolume().then((r1) => {
 
-	getFinalizedMarketAuctionsByDate().then((r1) => {
+		chart0.data = r1;
+		chart0.update();
+		document.getElementById("marketVolumeSpinner").remove();
 		
-		chart1.data = r1;
-		chart1.update();
-		document.getElementById("finalizedMarketAuctionsSpinner").remove();
+		getMarketStats().then((r2) => {
+		
+			chart1.data = r2;
+			chart1.update();
+			document.getElementById("marketStatsSpinner").remove();
+		})
 	})
-
  })
 
 }
