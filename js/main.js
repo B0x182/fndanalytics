a64c33db8bff4f603a44da70fe7adac9c1ee5b01
B0x182 <97475421+B0x182@users.noreply.github.com>
2022-01-18 22:08:46 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,21 @@
 window.onload = function (){
 
- var chart0 = new Chart(
-    document.getElementById('marketVolumeChart'),
-    getStackedBarChartConfig()
-  );
-
- var chart1 = new Chart(
-    document.getElementById('marketStatsChart'),
-    getBarChartConfig()
-  );
-
-
 getGASPriceHTMLTable().then((t) => {	
 		 document.getElementById("gasPriceTable").innerHTML=t;
 		})
+
+getNFTCardHTML("0x3b3ee1931dc30c1957379fac9aba94d1c48a5405-121713").then((c) => {	
+		 document.getElementById("nftcard0").innerHTML=c;
+		})
+		
+getNFTCardHTML("0x3b3ee1931dc30c1957379fac9aba94d1c48a5405-107617").then((c) => {	
+		 document.getElementById("nftcard1").innerHTML=c;
+		})		
+
+getNFTCardHTML("0x3b3ee1931dc30c1957379fac9aba94d1c48a5405-118255").then((c) => {	
+		 document.getElementById("nftcard2").innerHTML=c;
+		})
+
 
 //update all 20 sec
 var intervalId = window.setInterval(function(){
@@ -25,22 +27,5 @@ var intervalId = window.setInterval(function(){
 
 }, 20000);
 
-initFNDanalytics().then((r0) => {
- 
-	getMarketVolume().then((r1) => {
-
-		chart0.data = r1;
-		chart0.update();
-		document.getElementById("marketVolumeSpinner").remove();
-		
-		getMarketStats().then((r2) => {
-		
-			chart1.data = r2;
-			chart1.update();
-			document.getElementById("marketStatsSpinner").remove();
-		})
-	})
- })
- 
 
 }
