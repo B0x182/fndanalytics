73468dc3c8a8c81de73811030ffe2cb424384283
B0x182 <97475421+B0x182@users.noreply.github.com>
2022-01-25 23:52:29 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 window.onload = function (){
 
+var _updateInt = 20000;
+var _tickrate = 1000;
+
 getGASPriceHTMLTable().then((t) => {	
+		
 		 document.getElementById("gasPriceTable").innerHTML=t;
+		 document.getElementById("gasPriceTableSpinner").remove();
+		   update();
 		})
 
 getNFTCardHTML("0x3b3ee1931dc30c1957379fac9aba94d1c48a5405-121713").then((c) => {	
@@ -12,20 +18,43 @@ getNFTCardHTML("0x3b3ee1931dc30c1957379fac9aba94d1c48a5405-107617").then((c) => 
 		 document.getElementById("nftcard1").innerHTML=c;
 		})		
 
-getNFTCardHTML("0x3b3ee1931dc30c1957379fac9aba94d1c48a5405-118255").then((c) => {	
+getNFTCardHTML("0x3b3ee1931dc30c1957379fac9aba94d1c48a5405-121948").then((c) => {	
 		 document.getElementById("nftcard2").innerHTML=c;
 		})
 
 
-//update all 20 sec
-var intervalId = window.setInterval(function(){
- 
-  getGASPriceHTMLTable().then((t) => {
-		
-		 document.getElementById("gasPriceTable").innerHTML=t;
-		})
 
-}, 20000);
+function update(){	
+	
+	var i = 0;
+	var width = 100;
+	var elem = document.getElementById("progressbar");
+	elem.style.width = width + "%";
+     elem.innerHTML = (_updateInt - i)/_tickrate + " seconds";
+	
+    
+    var id = setInterval(frame, _tickrate);
+	
+	function frame() {
+      if (i == _updateInt) {
+     
+	 clearInterval(id);
+	
+		getGASPriceHTMLTable().then((t) => {
+		 document.getElementById("gasPriceTable").innerHTML=t;
+		 update();
+		})
+		
+      } else {
+		  i = i + _tickrate;
+	
+        width =   (_updateInt - i) / _updateInt * 100;
+        elem.style.width = width + "%";
+        elem.innerHTML = (_updateInt - i)/_tickrate + " seconds";
+      }
+    }
+	
+}
 
 
 }
