978cb66e5a7e2ec773a1bb9da56d098f792f68e3
B0x182 <97475421+B0x182@users.noreply.github.com>
2022-01-30 17:44:11 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,12 +3,38 @@ window.onload = function (){
 var _updateInt = 20000;
 var _tickrate = 1000;
 
-getGASPriceHTMLTable().then((t) => {	
+
+ getGASPriceHTMLTable().then((t) => {	
 		
 		 document.getElementById("gasPriceTable").innerHTML=t;
 		 document.getElementById("gasPriceTableSpinner").remove();
-		   update();
+		 update();
+  })
+
+
+ getBlockHash().then((r0) => {
+ 
+	getNFTsMintedStats24H().then((r1) => {
+		
+		document.getElementById("stats2").innerHTML=r1;
+	    document.getElementById("stats2sp").remove();
+		
+		getAuctionsStartedStats24H().then((r2) => {
+			
+			document.getElementById("stats3").innerHTML=r2;
+			document.getElementById("stats3sp").remove();
+			
+			getMarketVolumeStats24H().then((r3) => {
+		
+				document.getElementById("stats1").innerHTML=r3;
+				document.getElementById("stats1sp").remove();
+			})
+
 		})
+
+	})
+ })
+
 
 getNFTCardHTML("0x3b3ee1931dc30c1957379fac9aba94d1c48a5405-121713").then((c) => {	
 		 document.getElementById("nftcard0").innerHTML=c;
@@ -23,12 +49,11 @@ getNFTCardHTML("0x3b3ee1931dc30c1957379fac9aba94d1c48a5405-121948").then((c) => 
 		})
 
 
-
 function update(){	
 	
 	var i = 0;
 	var width = 100;
-	var elem = document.getElementById("progressbar");
+	var elem = document.getElementById("gasprogress");
 	elem.style.width = width + "%";
      elem.innerHTML = (_updateInt - i)/_tickrate + " seconds";
 	
